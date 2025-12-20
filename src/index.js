e8c02ae8f2d0d9b3275e0bac2b18282a05a7104e
彭涉涛 <43326761+psterman@users.noreply.github.com>
2025-12-20 22:37:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+<script src="https://cdn.tailwindcss.com"></script>
+
+<!-- 配置 Tailwind 允许通过 class 切换暗色模式 -->
+<script>
+  tailwind.config = {
+    darkMode: 'class',
+  }
+</script>
